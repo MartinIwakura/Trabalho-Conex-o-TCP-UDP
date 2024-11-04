59668a6f1717e2fd84c589fc9feee051fd38e106
Martins <134743782+MartinIwakura@users.noreply.github.com>
2024-11-04 19:46:28 -0300

    Update mainSerUDP.c

--- mainSerUDP.c
+++ mainSerUDP.c
@@ -6,9 +6,9 @@
 #define BUFFER_SIZE 128
 #define FINALIZAR "#fim"
 
-int local_socket;
-struct sockaddr_in local_address, remote_address;
 WSADATA wsa_data;
+SOCKET local_socket;
+struct sockaddr_in local_address, remote_address;
 
 void msg_err_exit(const char *msg) {
     fprintf(stderr, "%s\n", msg);
@@ -17,18 +17,16 @@ void msg_err_exit(const char *msg) {
 }
 
 int main() {
-    // Inicia o Winsock 2.0
     if (WSAStartup(MAKEWORD(2, 0), &wsa_data) != 0)
-        msg_err_exit("WSAStartup() falhou");
+        msg_err_exit("Erro ao iniciar o Winsock");
 
-    // Cria o socket UDP
     local_socket = socket(AF_INET, SOCK_DGRAM, 0);
     if (local_socket == INVALID_SOCKET) {
-        msg_err_exit("socket() falhou");
+        msg_err_exit("Erro ao criar o socket UDP");
     }
 
     unsigned short local_port;
-    printf("Porta local: ");
+    printf("Informe a porta local: "); 
     scanf("%hu", &local_port);
     fflush(stdin);
 
@@ -39,26 +37,26 @@ int main() {
 
     if (bind(local_socket, (struct sockaddr *)&local_address, sizeof(local_address)) == SOCKET_ERROR) {
         closesocket(local_socket);
-        msg_err_exit("bind() falhou");
+        msg_err_exit("Erro ao associar o socket à porta");
     }
 
     printf("Servidor aguardando pacotes...\n");
 
-    char buffer[BUFFER_SIZE];
     FILE *file = fopen("arquivo_recebido.txt", "wb");
     if (file == NULL) {
-        msg_err_exit("Erro ao criar o arquivo");
+        msg_err_exit("Erro ao criar o arquivo para salvar os dados recebidos");
     }
 
     int remote_length = sizeof(remote_address);
     int bytes_received;
+    char buffer[BUFFER_SIZE];
 
     while (1) {
         memset(buffer, 0, BUFFER_SIZE);
         bytes_received = recvfrom(local_socket, buffer, BUFFER_SIZE, 0, 
                                   (struct sockaddr *)&remote_address, &remote_length);
         if (bytes_received == SOCKET_ERROR) {
-            msg_err_exit("recvfrom() falhou");
+            msg_err_exit("Erro ao receber dados");
         }
 
         printf("Recebido %d bytes: %s\n", bytes_received, buffer);
@@ -75,5 +73,6 @@ int main() {
     closesocket(local_socket);
     WSACleanup();
     printf("Arquivo salvo como 'arquivo_recebido.txt'.\n");
+
     return 0;
 }
